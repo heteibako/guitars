5a1232938d3e159b12cf160270c463747caaddb2
heteibako <hetei_bako@yahoo.com>
2021-03-05 21:20:36 +0100

    again

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://guitars.vercel.app/api/*',
+        destination: 'https://guitars.vercel.app/*',
       },
     ];
   },
