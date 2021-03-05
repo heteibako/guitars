54ec7945947af4b264cf3bc864bdd77f9452178c
heteibako <hetei_bako@yahoo.com>
2021-03-05 21:13:47 +0100

    async rewrites

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   env: {
     MONGO_URI: 'mongodb://localhost:27017/guitarstore',
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
+      },
+    ];
+  },
 };
