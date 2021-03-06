2f25d778a8a7496bd79a245a95829caa70f7d861
heteibako <hetei_bako@yahoo.com>
2021-03-06 19:37:07 +0100

    fixed api

--- hooks/useAddGuitar.js
+++ hooks/useAddGuitar.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import axios from 'axios';
 
 export const useAddGuitar = async (data) => {
   try {
-    await axios.post(`${process.env.API_URL}/api/guitars/`, data);
+    await axios.post(`/api/guitars/`, data);
   } catch (error) {
     console.log(error);
   }
