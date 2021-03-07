a1522ab21b32be2abc16680016a802ead9ec19ef
heteibako <hetei_bako@yahoo.com>
2021-03-07 08:53:07 +0100

    added breakpoints

--- components/HalfColumn.js
+++ components/HalfColumn.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { device } from './helpers/deviceSizes';
 
 export const HalfColumn = styled.div`
   width: 50%;
@@ -6,4 +7,16 @@ export const HalfColumn = styled.div`
   margin: 0 auto;
   display: flex;
   justify-content: ${(props) => (props.center ? 'center' : 'flex - start')};
+  @media ${device.laptopL} {
+    width: 50%;
+  }
+  @media ${device.mobileS} {
+    width: 100%;
+  }
+  @media ${device.mobileM} {
+    width: 100%;
+  }
+  @media ${device.mobileL} {
+    width: 100%;
+  }
 `;
--- components/Wrapper.js
+++ components/Wrapper.js
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
-
+import { device } from './helpers/deviceSizes';
 export const Wrapper = styled.section`
   padding: 4em;
   display: flex;
-  flex-direction: row;
+
+  @media ${device.laptopL} {
+    flex-direction: row;
+  }
+  @media ${device.laptopM} {
+    flex-direction: row;
+  }
+  @media ${device.mobileS} {
+    flex-direction: column;
+  }
+  @media ${device.mobileM} {
+    flex-direction: column;
+  }
+  @media ${device.mobileL} {
+    flex-direction: column;
+  }
 `;
