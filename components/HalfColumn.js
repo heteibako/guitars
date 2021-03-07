0df0408c2ed6132c5771c34407445caf8d53fec3
heteibako <hetei_bako@yahoo.com>
2021-03-07 08:35:36 +0100

    added image

--- components/HalfColumn.js
+++ components/HalfColumn.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const HalfColumn = styled.div`
   width: 50%;
   padding: 1rem;
+  margin: 0 auto;
+  display: flex;
+  justify-content: ${(props) => (props.center ? 'center' : 'flex - start')};
 `;
--- pages/guitars/[id].jsx
+++ pages/guitars/[id].jsx
@@ -4,22 +4,34 @@ import axios from 'axios';
 import { motion } from 'framer-motion';
 import { Wrapper } from '../../components/Wrapper';
 import { HalfColumn } from '../../components/HalfColumn';
+import Image from 'next/image';
 
-const Guitar = ({ guitar: { body, name, strings, neck, neckProfile, pickups } }) => {
+const Guitar = ({ guitar: { body, name, strings, neck, neckProfile, pickups, image } }) => {
   return (
     <Wrapper>
-      <HalfColumn>
+      <HalfColumn center>
         <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+          <Image src={`/images/${image}.png`} alt='me' width='200' height='600' />
           <h1>{name}</h1>
         </motion.div>
       </HalfColumn>
       <HalfColumn>
         <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-          <p>{body}</p>
-          <p>{strings}</p>
-          <p>{neck}</p>
-          <p>{neckProfile}</p>
-          <p>{pickups}</p>
+          <p>
+            body: <strong>{body}</strong>{' '}
+          </p>
+          <p>
+            strings: <strong>{strings}</strong>
+          </p>
+          <p>
+            neck: <strong>{neck}</strong>
+          </p>
+          <p>
+            neck profile: <strong>{neckProfile}</strong>
+          </p>
+          <p>
+            pickups: <strong>{pickups}</strong>
+          </p>
         </motion.div>
       </HalfColumn>
     </Wrapper>
