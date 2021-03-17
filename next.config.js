9bf51dee10880550ab51d3049d776bf2cb7ebb15
heteibako <hetei_bako@yahoo.com>
2021-03-17 20:35:23 +0100

    no netlify build

--- next.config.js
+++ next.config.js
@@ -7,18 +7,6 @@ module.exports = {
   env: {
     MONGO_URI: process.env.MONGO_URI,
   },
-  images: {
-    loader: 'imgix',
-    path: 'https://silly-allen-dbfbc7.netlify.app/',
-  },
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/guitars': { page: '/guitars' },
-      '/guitars/[slug]': { page: '/guitars/[slug]' },
-      // etc...
-    };
-  },
   plugins: [['styled-components', { ssr: true, displayName: true, preprocess: false }]],
 };
 
